2cc030b83c1be44d5a0072587f945a919fa4080f
Janix4000 <janix4000@gmail.com>
2022-06-20 12:46:41 +0200

    change up do var dependent

--- lemings/game.py
+++ lemings/game.py
@@ -23,7 +23,7 @@ next_move = {
 def next_game_move(game_state: GameState, board: Board, move: str):
     state = move
     final_pos = game_state.pos
-    will_fall = True
+    fall = 1
     while state != 's':
         next_pos = final_pos + next_move[state]
         next_obstacle = board[next_pos[0]][next_pos[1]]
@@ -32,17 +32,17 @@ def next_game_move(game_state: GameState, board: Board, move: str):
             state = 's'
         else:
             final_pos = next_pos
-            if next_obstacle == '^':
+            if next_obstacle.isdigit():
                 state = 'u'
-                will_fall = False
+                fall -= int(next_obstacle)
             elif next_obstacle == '*':
                 state = 's'
                 game_state.state = 'dead'
             else:
                 state = 's'
-        if game_state.state != 'dead' and will_fall and state != 'u':
-            state = 'd'
-            will_fall = False
+        if game_state.state != 'dead' and fall != 0:
+            state = 'd' if fall > 0 else 'u'
+            fall -= abs(fall) // fall
 
     game_state.pos = final_pos
     if game_state.state != 'dead' and len(board) - 2 == final_pos[0] and len(board[0]) - 2 == final_pos[1]:
@@ -54,9 +54,9 @@ if __name__ == '__main__':
     board = np.array([
         '##########',
         '#........#',
-        '#.....^..#',
-        '#..###^..#',
-        '#***..^..#',
+        '#........#',
+        '#..###...#',
+        '#***..4..#',
         '##########',
     ])
 
